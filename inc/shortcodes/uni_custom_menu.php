6f4703bd8305fe38dea8462d0bf5dbc0427887bb
Hòa <hoanqhd@gmail.com>
2020-10-14 23:21:27 +0700

    Update code

--- inc/shortcodes/uni_custom_menu.php
+++ inc/shortcodes/uni_custom_menu.php
@@ -15,41 +15,39 @@ function shortcode_uni_custom_menu($atts, $content = null, $tag) {
     // Get menu.
     $nav_menu = ! empty( $nav_menu ) ? wp_get_nav_menu_object( $nav_menu ) : false;
 
-    if ( ! $nav_menu ) {
-        return;
+    if ( ! empty( $nav_menu ) ) {
+        $format = current_theme_supports( 'html5', 'navigation-widgets' ) ? 'html5' : 'xhtml';
+
+        /**
+         * Filters the HTML format of widgets with navigation links.
+         *
+         * @since 5.5.0
+         *
+         * @param string $format The type of markup to use in widgets with navigation links.
+         *                       Accepts 'html5', 'xhtml'.
+         */
+        $format = apply_filters( 'navigation_widgets_format', $format );
+
+        if ( 'html5' === $format ) {
+            // The title may be filtered: Strip out HTML and make sure the aria-label is never empty.
+            $title      = trim( strip_tags( $title ) );
+            $aria_label = $title ? $title : $default_title;
+
+            $nav_menu_args = array(
+                'fallback_cb'          => '',
+                'menu'                 => $nav_menu,
+                'container'            => 'nav',
+                'container_aria_label' => $aria_label,
+                'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            );
+        } else {
+            $nav_menu_args = array(
+                'fallback_cb' => '',
+                'menu'        => $nav_menu,
+            );
+        }
+        wp_nav_menu( apply_filters( 'widget_nav_menu_args', $nav_menu_args, $nav_menu, $args, $instance ) );
     }
-
-    $format = current_theme_supports( 'html5', 'navigation-widgets' ) ? 'html5' : 'xhtml';
-
-    /**
-     * Filters the HTML format of widgets with navigation links.
-     *
-     * @since 5.5.0
-     *
-     * @param string $format The type of markup to use in widgets with navigation links.
-     *                       Accepts 'html5', 'xhtml'.
-     */
-    $format = apply_filters( 'navigation_widgets_format', $format );
-
-    if ( 'html5' === $format ) {
-        // The title may be filtered: Strip out HTML and make sure the aria-label is never empty.
-        $title      = trim( strip_tags( $title ) );
-        $aria_label = $title ? $title : $default_title;
-
-        $nav_menu_args = array(
-            'fallback_cb'          => '',
-            'menu'                 => $nav_menu,
-            'container'            => 'nav',
-            'container_aria_label' => $aria_label,
-            'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-        );
-    } else {
-        $nav_menu_args = array(
-            'fallback_cb' => '',
-            'menu'        => $nav_menu,
-        );
-    }
-    wp_nav_menu( apply_filters( 'widget_nav_menu_args', $nav_menu_args, $nav_menu, $args, $instance ) );
 
     echo '</div>';
 
